e9a6e32370ded4941ec7d061af8421e106164e8f
Jyoti Kumari <jyoti02.nitt@gmail.com>
2021-07-15 17:06:25 +0530

    first commit

---
+++ src/Components/Landing/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Landing.css';
+
+const Landing = () => {
+    return (
+        <div className="Landing-container">
+            <p> I am Landing page</p>
+        </div>
+    );
+};
+
+
+export default Landing;
